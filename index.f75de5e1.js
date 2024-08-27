71acc2cd1e1698c28faeb77cd251775d2a803590
Daria Nastas <bycinska@gmail.com>
2024-08-27 17:59:54 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,27 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", ()=>{
+    const table = document.querySelector("table");
+    const headers = table.querySelectorAll("th");
+    const tbody = table.querySelector("tbody");
+    headers.forEach((header, index)=>{
+        header.addEventListener("click", ()=>{
+            sortTableByColumn(table, index);
+        });
+    });
+    function sortTableByColumn(_table, columnIndex) {
+        const rows = [
+            ...tbody.querySelectorAll("tr")
+        ];
+        rows.sort((rowA, rowB)=>{
+            const cellA = rowA.cells[columnIndex].innerText.toLowerCase();
+            const cellB = rowB.cells[columnIndex].innerText.toLowerCase();
+            const valueA = columnIndex === 2 ? parseInt(cellA) : columnIndex === 3 ? parseFloat(cellA.replace("$", "")) : cellA;
+            const valueB = columnIndex === 2 ? parseInt(cellB) : columnIndex === 3 ? parseFloat(cellB.replace("$", "")) : cellB;
+            return valueA > valueB ? 1 : valueA < valueB ? -1 : 0;
+        });
+        tbody.innerHTML = "";
+        rows.forEach((row)=>tbody.appendChild(row));
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
